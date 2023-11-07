aa0ee38da7369d787e7d6ae3484b9e63e72ce34d
jcanchucaja <jcanchucaja@yahoo.com.mx>
2023-11-07 11:34:39 -0500

    Primera subida
    Primera subida

---
+++ proyectoRedisCache/src/main/java/com/redis/cache/ProyectoRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.redis.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRedisCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRedisCacheApplication.class, args);
+	}
+
+}
